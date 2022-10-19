1162a748b7560f0ebe40543861607847f2d1f37f
BrotRooti <sepp2017.huber@gmail.com>
2022-10-19 14:38:05 +0200

    Changed Files

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+
 int main() {
+    int iIQ;
+    iIQ = 100;
     printf("Hello, World!\n");
+    printf("I have an IQ of %d", iIQ);
     return 0;
 }
---
+++ if_else.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main() {
+    int iZahl;
+
+    iZahl=10;
+
+    if (iZahl==10)
+    {
+        printf("Zahl ist 10");
+    }
+    else
+    {
+        printf("nein..");
+    }
+    printf("Hello, World!\n");
+    return 0;
+}
